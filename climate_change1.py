724ec2514a9b6e0019c8a4f9bc1900b905442d23
Aakhashd <117091401+Aakhashd@users.noreply.github.com>
2022-12-03 18:09:19 +0000

    bar_plot_total_population

--- climate_change1.py
+++ climate_change1.py
@@ -5,4 +5,26 @@ Created on Sat Dec  3 14:26:37 2022
 
 @author: aakhashd
 """
+import matplotlib.pyplot as plt
+import pandas as pd
 
+# Reading data from the Csv file 
+df1=pd.read_csv("climate_change2.csv")
+df2=df1.replace(' ',0)
+countries=['France','Australia','Brazil','China',
+           'United Kingdom','India','United States']
+filt=df2['Country Name'].isin(countries)
+df2f=df2.loc[filt]
+print(df2f)
+df2_group=df2f.groupby(['Country Name'], as_index=False).sum()
+print(df2_group)
+
+df2t=df2_group.T
+print(df2t)
+
+plt.figure(figsize=(30,10))
+dataframe=df2_group.iloc[:,[0,1,11,21,31,41,51,61,62]]
+print(dataframe)
+
+dataframe.plot(kind="bar", x= 'Country Name', rot=0, align='center',width=1.5)
+plt.xticks(rotation = 30)

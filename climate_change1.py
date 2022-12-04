c4b0e6e73d7d239710300b3ab9802f5e0dec2b29
Aakhashd <117091401+Aakhashd@users.noreply.github.com>
2022-12-04 14:37:03 +0000

    code_changes_and_fixed_the_variable_names

--- climate_change1.py
+++ climate_change1.py
@@ -9,22 +9,47 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Reading data from the Csv file 
-df1=pd.read_csv("climate_change2.csv")
-df2=df1.replace(' ',0)
-countries=['France','Australia','Brazil','China',
-           'United Kingdom','India','United States']
-filt=df2['Country Name'].isin(countries)
-df2f=df2.loc[filt]
-print(df2f)
-df2_group=df2f.groupby(['Country Name'], as_index=False).sum()
-print(df2_group)
+def reading_csv(filename):
+    csv_dataframe=pd.read_csv(filename)
+    cleaned_dataframe=csv_dataframe.replace(' ',0)
+    countries=['France','Australia','Brazil','China',
+               'United Kingdom','India']
+    filt=cleaned_dataframe['Country Name'].isin(countries)
+    filtered_dataframe=cleaned_dataframe.loc[filt]
+    filtered_dataframe=filtered_dataframe.groupby(['Country Name'], 
+                                                  as_index=False).sum()
+    #print(df2_group)
+    #df2t=filtered_dataframe.T
+    #print(df2t)
+    return filtered_dataframe
 
-df2t=df2_group.T
-print(df2t)
+def climate_change_barchart(filename):
+    climate_change_df=reading_csv(filename)
+    plt.figure(figsize=(30,10))
+    dataframe=climate_change_df.iloc[:,[0,1,21,41,51,61,62]]
+    dataframe.plot(kind="bar", x= 'Country Name', rot=0, align='center',width=1.5)
+    plt.xticks(rotation = 20)
+    plt.title("Total Estimated climate change (1960-2021)")
+    plt.ylabel("Amount of climate changes")
 
-plt.figure(figsize=(30,10))
-dataframe=df2_group.iloc[:,[0,1,11,21,31,41,51,61,62]]
-print(dataframe)
 
-dataframe.plot(kind="bar", x= 'Country Name', rot=0, align='center',width=1.5)
-plt.xticks(rotation = 30)
+def co2_emission_linechart(filename):
+    co2_emission=reading_csv(filename)
+    plt.Figure(figsize=(10,10))
+    co2_emission.plot(x="Country Name", y=["1960","1980","2000","2010","2020","2021"],
+                  kind="line", linestyle='-.')
+    plt.title("Total Estimated number of co2 emission (1960-2021)")
+    plt.ylabel("total values for co2 emission")
+
+def renewable_enegy_linechart(filename):
+    renewable_energy=reading_csv(filename)
+    print(renewable_energy)
+    plt.figure(figsize=(20,20))
+    renewable_energy.plot(x="Country Name", y=["1960","1980","2000","2010","2020","2021"],
+                          kind="line", linestyle='-.')
+    plt.title("Total Estimated number of Renewable energy(1960-2021)")
+    plt.ylabel("total values for Renewable energy")
+climate_change_barchart("climate_change2.csv")
+co2_emission_linechart("co2_emmision.csv")
+renewable_enegy_linechart("renewable_enegy.csv")
+plt.show()

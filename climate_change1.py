64a64e9e569287d657c2178ba929ced0a3f852f2
Aakhashd <117091401+Aakhashd@users.noreply.github.com>
2022-12-11 13:34:18 +0530

    code_changes_committed

--- climate_change1.py
+++ climate_change1.py
@@ -7,6 +7,7 @@ Created on Sat Dec  3 14:26:37 2022
 """
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 # Reading data & cleaning data from the Csv file 
 def reading_csv(filename):
@@ -21,7 +22,7 @@ def reading_csv(filename):
     filtered_dataframe=filtered_dataframe.groupby(
                         ['Country Name'], as_index=False).sum()
     final_filtered_Dataframe=filtered_dataframe.filter(
-        items=['Country Name','1960', '1980','2000','2010','2018','2019'])
+        items=['Country Name', '1980','2000','2010','2019'])
     # Transposing the data so plot the graph based on countries
     transpose_data=final_filtered_Dataframe.set_index(['Country Name']).T
     return (final_filtered_Dataframe,transpose_data)
@@ -30,24 +31,22 @@ def reading_csv(filename):
 def population(filename):
     # Fetcing the dataframe for population of data
     population,population_transposeddf=reading_csv(filename)
-    plt.figure(figsize=(30,10))
+    plt.figure(figsize=(10,18))
     #Plotting the population data on bar plot
-    population.plot(kind="bar", x='Country Name', rot=0, align='center',
+    population.plot(kind="bar", x='Country Name', rot=10, align='center',
                     width=0.5)
-    plt.xticks(rotation = 20)
-    plt.title("Total Estimated population (1960-2019)")
-    plt.ylabel("Population growth (annual %)")
+    plt.title("Urban population (1980-2019)")
+    plt.ylabel("Population growth %")
     plt.savefig("population.png")
 
 #defining the function for energy usage  bar chart
 def energy_use(filename):
     # Fetcing the dataframe for Energy use of data
     energy_use,energy_use_transposeddf=reading_csv(filename)
-    plt.figure(figsize=(30,10))
-    energy_use.plot(kind="bar", x='Country Name', rot=0, align='center',
+    plt.figure(figsize=(10,18))
+    energy_use.plot(kind="bar", x='Country Name', rot=10, align='center',
                       width=0.5)
-    plt.xticks(rotation = 20)
-    plt.title("Estimated energy usage (1960-2019)")
+    plt.title("Estimated energy usage (1980-2019)")
     plt.ylabel("Energy use (kg of oil equivalent per capita)")
     plt.savefig("energy_use.png")
 
@@ -58,7 +57,7 @@ def co2_emission_linechart(filename):
     plt.legend()
     # ploting the transposed dataframe with line chart
     co2_transposed.plot(kind="line", linestyle='-.', 
-             title ="Estimated amount of co2 emission (1960-2019)", 
+             title ="Estimated amount of co2 emission (1980-2019)", 
              ylabel="Amount of co2 emission (kt)", xlabel="Period")
     plt.savefig("co2_emission.png")
 
@@ -68,7 +67,7 @@ def methane_emission(filename):
     plt.figure(figsize=(20,20))
     # ploting the transposed dataframe with line chart
     methane_emissiondf.plot(kind="line", linestyle='-.')
-    plt.title("Estimated amount of Methane emmission(1960-2019)")
+    plt.title("Estimated amount of Methane emmission(1980-2019)")
     plt.ylabel("Amount of Methane emissions (kt)")
     plt.xlabel("Period")
     plt.savefig("methane_emission.png")
@@ -85,16 +84,19 @@ def map_corr(df, size=10):
     # setting ticks to column names
     plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
     plt.yticks(range(len(corr.columns)), corr.columns)
+    for i,value in enumerate(corr.columns): #loop to add correlation value to the heatmap
+        for j in range(len(corr.columns)):
+            ax.text(j,i,round(corr[value][j],2),ha="center",color="w") 
     plt.savefig("heat_map.png",dpi =400)
 
 # defining a function for ploting heatmap
 def heatmap(filename):
-    d=pd.read_csv(filename)
-    df1=d.replace(' ',0)
+    heat_mapcsv=pd.read_csv(filename)
+    heat_df=heat_mapcsv.replace(' ',0)
     #Filtering the data based on country
     countries=['China']
-    filt=df1['Country Name'].isin(countries)
-    df2=df1.loc[filt]
+    filt=heat_df['Country Name'].isin(countries)
+    heat_df=heat_df.loc[filt]
     #Filtering the data based on Indicators
     indicator_name=['Urban population','CO2 emissions (kt)'
                     ,'Forest area (sq. km)','Agricultural land (sq. km)'
@@ -103,22 +105,23 @@ def heatmap(filename):
                     'Methane emissions (kt of CO2 equivalent)',
                     'Total greenhouse gas emissions (kt of CO2 equivalent)'
                     'Population, total','Agricultural land (sq. km)']
-    filtered=df2['Indicator Name'].isin(indicator_name)
-    df=df2.loc[filtered]
+    filtered=heat_df['Indicator Name'].isin(indicator_name)
+    df=heat_df.loc[filtered]
     df.drop(['Country Name', 'Country Code'], axis=1, inplace=True)
-    df9=df[['Indicator Name','1960','1980','2000','2010','2018','2019']]
-    dft=df9.set_index(['Indicator Name']).T
-    map_corr(dft)
+    #Droping the columns values
+    indiacator_df=df[['Indicator Name','1980','2000','2010','2018','2019']]
+    #Transposing the Dataframe
+    heatmap_transposedf=indiacator_df.set_index(['Indicator Name']).T
+    map_corr(heatmap_transposedf)
     plt.show()
-    dft = dft.fillna(0)
+    heatmap_transposedf = heatmap_transposedf.fillna(0)
  
 heatmap("climate_change2.csv")
-population("population1.csv")
+population("urban_population.csv")
 energy_use("energy_use.csv")
 co2_emission_linechart("co2_emmision.csv")
 methane_emission("methane_emission.csv")
 plt.show()
- 
 
 
 
